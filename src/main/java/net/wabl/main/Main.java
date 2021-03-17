380f27390bdd086c321c416a316ab7b64c203ffa
Sebastian <Sebastian@Strobl.net>
2021-03-17 16:20:04 +0100

    Add: JDA

--- src/main/java/net/wabl/main/Main.java
+++ src/main/java/net/wabl/main/Main.java
@@ -1,7 +1,25 @@
 package net.wabl.main;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Discord!");
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = JDABuilder.createDefault(args[0]);
+
+        // Disable parts of the cache
+        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+        // Enable the bulk delete event
+        builder.setBulkDeleteSplittingEnabled(false);
+        // Disable compression (not recommended)
+        builder.setCompression(Compression.NONE);
+        // Set activity (like "playing Something")
+        builder.setActivity(Activity.watching("TV"));
+
+        builder.build();
     }
 }
