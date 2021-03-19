951adc2010bf99ec965fb5e5b21844851ffd7f35
Sebastian <Sebastian@Strobl.net>
2021-03-19 20:23:26 +0100

    Add: .env support

--- src/main/java/net/wabl/main/Main.java
+++ src/main/java/net/wabl/main/Main.java
@@ -1,14 +1,16 @@
 package net.wabl.main;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
 
 import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        JDA jda = JDABuilder.createDefault("token").build();
+        Dotenv dotenv = Dotenv.load();
+
+        JDA jda = JDABuilder.createDefault(dotenv.get("TOKEN")).build();
         jda.addEventListener(new Embed());
     }
 }
