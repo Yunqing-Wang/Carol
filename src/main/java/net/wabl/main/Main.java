67033ea40fe869d9f7be4269827fc2a2c325cc6b
Yunqing <smilerice@hotmail.com>
2021-03-19 17:06:04 +0100

    Change: token

--- src/main/java/net/wabl/main/Main.java
+++ src/main/java/net/wabl/main/Main.java
@@ -8,7 +8,7 @@ import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        JDA jda = JDABuilder.createDefault("ODIxNDgzNzk3NjIyNjg1Njk2.YFEYZw.ZC-vI8NOZlH_rbgXlTsfRyM8qoc").build();
+        JDA jda = JDABuilder.createDefault("token").build();
         jda.addEventListener(new Embed());
     }
 }
